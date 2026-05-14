5e0b4bbe2ecaa1715a3479a82174c583fce7dc95
Jambione <jon@jbrasfield.com>
2026-05-14 04:54:40 -0400

    windows agnet fix?

--- windows_agent.py
+++ windows_agent.py
@@ -291,6 +291,8 @@ class AgentHandler(BaseHTTPRequestHandler):
         self.send_header("Access-Control-Allow-Origin",  "*")
         self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        # Allow Private Network Access from remote HTTPS origins (required for browser security policy)
+        self.send_header("Access-Control-Allow-Private-Network", "true")
 
     def do_OPTIONS(self):
         self.send_response(204)
